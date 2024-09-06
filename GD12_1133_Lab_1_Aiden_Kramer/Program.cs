24892085e1ce1d703ca7be6bcaa92105c73352e3
rager-code <nuclearshock256@gmail.com>
2024-09-06 12:16:30 -0700

    Added Visual Studio Files

---
+++ GD12_1133_Lab_1_Aiden_Kramer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GD12_1133_Lab_1_Aiden_Kramer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Aiden!");
+        }
+    }
+}
